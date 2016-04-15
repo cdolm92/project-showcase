5708c1cd3522062f7d755a89a2bbaede0799e0ab
cdolm92 <cdolm92@gmail.com>
2016-04-15 00:24:32 -0400

    embedded FeedVC in NavigationController added table view and custom tableview cells to this VC, Custom TVC are complete

---
+++ project-showcase/FeedVC.swift
@@ -0,0 +1,47 @@
+//
+//  FeedVC.swift
+//  project-showcase
+//
+//  Created by Christella on 4/14/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+    
+    
+    
+
+    
+
+}
---
+++ project-showcase/PostCell.swift
@@ -0,0 +1,35 @@
+//
+//  PostCell.swift
+//  project-showcase
+//
+//  Created by Christella on 4/14/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = 20
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
